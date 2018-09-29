de354002f0521a08a9f1401017b8f5222138481c
Ramy Elshenawy <r.elshenawy@gmail.com>
2018-09-29 08:02:38 -0400

    adding slider.js and time.js, moving bayaan.html to index

---
+++ common/slider.js
@@ -0,0 +1,65 @@
+function Divs(divs) {
+  if (divs.length < 2) return;
+
+  now = divs.filter(':visible');
+  next = now.length && now.next().length ? now.next() : divs.first();
+  speed = 1000;
+
+  now.fadeOut(speed);
+  next.fadeIn(speed);
+}
+
+$(function () {
+  setInterval(function () { Divs($('#announcements div')) }, 10000);
+  setInterval(function () { Divs($('#benefits div')) }, 13000);
+});
+
+function loadContent(user, album) {
+  var link = 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=5c053e417b99b7fe023ee6554a95c25b&photoset_id='+album+'&user_id='+user+'&format=json';
+  console.log(link);
+  jQuery.get(link, "jsonp");
+
+}
+
+function loadBenefits() {
+  var parentDiv = document.getElementById("benefits");
+  benefits = $.get("benefits.json?2").success( function(data) { 
+    data.list.forEach(function(benefit) {
+      var div = document.createElement('div');
+      var content =  document.createElement('p');
+      var source =  document.createElement('p');
+      div.style.marginTop = 30 - benefit.content.length / 10;
+      content.className = 'benefit-content';
+      content.textContent = benefit.content;
+      source.className = 'benefit-source';
+      source.textContent = benefit.source;
+      div.appendChild(content);
+      div.appendChild(source);
+      randomIndex = Math.floor(Math.random() * parentDiv.childElementCount);
+      parentDiv.insertBefore(div, parentDiv[randomIndex]);
+    });
+    $("#benefits div").next().fadeOut();
+    
+  });
+
+}
+
+function jsonFlickrApi(data) {
+    var photos = data.photoset.photo;
+    
+    for(var i = 0; i < photos.length; i++) {
+      $("#announcements").append("<div class=\"slide\"><img src=\""+ getLink(photos[i]) + "\"/></div>");
+      if(photos[i].title != "") {
+        $("#classes").append("<div><h2>"+photos[i].title+"</h2></div>"); 
+      }
+    }
+    $('#announcements div').next().fadeOut();
+
+}
+
+function getLink(photo) {
+  console.log(photo);
+  return "https://farm" + photo.farm +
+         ".staticflickr.com/" + photo.server + 
+         "/" + photo.id + "_" + photo.secret + "_h.jpg";
+}
---
+++ common/time.js
@@ -0,0 +1,79 @@
+function setTimesWithTimes(timesFile, offsets) {
+
+  var day = getDay();
+
+  jQuery.get(timesFile, function(data){ 
+    var startOfLine = data.indexOf(day+"_");
+    var offset = (day+"_").length;
+    var endOfLine = data.indexOf(day+1+"_");
+    var line = data.substring(startOfLine+offset, endOfLine);
+    var x = line.split(",");
+    $("#fajrTime").html(getIqamahTime(x[0], offsets[0]));
+    $("#dhuhrTime").html(getIqamahTime(x[1], offsets[1]));
+    $("#asrTime").html(getIqamahTime(x[2], offsets[2]));
+    $("#maghribTime").html(getIqamahTime(x[3], offsets[3]));
+    $("#ishaTime").html(getIqamahTime(x[4], offsets[4]));
+  });
+}
+
+function setTimes() {
+  setTimesWithTimes('/announcements/times.csv');
+}
+
+function getDay(){
+  var now = new Date();
+  var start = new Date(now.getFullYear(), 0, 1);
+  var diff = now - start;
+  var oneDay = 1000 * 60 * 60 * 24;
+  var day = Math.ceil(diff / oneDay);
+  return day;
+}
+
+function setTodaysClass() {
+
+  var today = (new Date(Date.now())).getDay();
+  jQuery.get('/announcements/classes', function(data) {
+    var classList = jQuery.parseJSON(data);
+    for(var i = 0; i < classList[today].classes.length; i++) {
+      $("#classbar").append("<div class=\"classes special\"><br/>" +
+                            "<span class=\"title special\">" +
+                            "Time: "+classList[today].classes[i].time + "<br/><br/>" +
+                            classList[today].classes[i].class + "</div>");
+    }
+
+  });
+
+}
+
+function getIqamahTime(athanTime, offset) {
+  var x = athanTime.split(":");
+  var athan = new Date();
+  athan.setHours(x[0]);
+  athan.setMinutes(x[1]);
+  var iqamah = new Date();
+  iqamah.setTime(athan.getTime() + offset * 60000);
+
+  var hour = iqamah.getHours() != 12 ? iqamah.getHours() % 12 : 12;
+  var ampm = iqamah.getHours() < 12 ? "AM" : "PM";
+  var minute = iqamah.getMinutes() >= 10 ? iqamah.getMinutes() : "0" + iqamah.getMinutes();
+
+  return hour + ":" + minute + " " + ampm;
+}
+
+function setClock() {
+  var now = new Date();
+  $("#clock").html(getIqamahTime(now.getHours() + ":" + now.getMinutes(), -240));
+}
+
+
+function setDate() {
+  var o = {month: "long", day: "numeric", year : "numeric"};
+  $("#gregorianDate").html((new Intl.DateTimeFormat("en-US", o)).format());
+  $("#hijriDate").html(getIslamicDate());
+}
+
+function refreshDaily() {
+  if((new Date()).getHours() == 0) {
+    location.reload();
+  }
+}
