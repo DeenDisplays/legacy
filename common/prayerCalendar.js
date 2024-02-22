5b1973e4061ec60e1d8382018fe427405f829acf
Ramy Elshenawy <r.elshenawy@gmail.com>
2024-02-22 09:59:51 -0500

    initial prayer calendar POC

---
+++ common/prayerCalendar.js
@@ -0,0 +1,39 @@
+function fillCalendar() {
+    var PT = new PrayTimes('ISNA');
+
+    var row;
+    var cell;
+    var date = new Date();
+    var times;
+
+    var prayers = ["fajr", "dhuhr", "asr", "maghrib", "isha"];
+
+    for (let i = 1; i <= 30; i++) {
+        // Create a new table row
+        row = document.createElement('tr');
+        times = PT.getTimes(date, [40.73, -74.18], -5, 'auto', '12h');
+
+        addCell(row, i)
+
+        addCell(row, date.toLocaleDateString('en-US', {
+            month: 'short',
+            day: 'numeric',
+            }))
+
+
+        for (let k = 0; k < 5; k++) {
+            addCell(row,times[prayers[k]]);
+        }
+
+        // Add the row to the table
+        document.getElementById("calendar").appendChild(row);
+
+        date = new Date(date.getTime() + 24 * 60 * 60 * 1000);
+    }
+}
+
+function addCell(row, text) {
+    cell = document.createElement('td');
+    cell.textContent = text;
+    row.appendChild(cell);
+}
